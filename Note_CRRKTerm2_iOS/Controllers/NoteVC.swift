06af01b8a52fd850523a24c29535d874126b228f
Cem Safa <c0820726@mylambton.ca>
2021-09-15 13:37:11 -0400

    Added ViewControllers for every view in project

---
+++ Note_CRRKTerm2_iOS/Controllers/NoteVC.swift
@@ -0,0 +1,29 @@
+//
+//  NoteVC.swift
+//  Note_CRRKTerm2_iOS
+//
+//  Created by Cem Safa on 2021-09-15.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
